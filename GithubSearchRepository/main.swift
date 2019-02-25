d1e4e21946d0adc749cbdcc748830ae9bf0c1a2b
SHIMIZU Taku <shimizu.taku@gmail.com>
2019-02-25 15:28:43 +0900

    Initial Commit

---
+++ GithubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GithubSearchRepository
+//
+//  Created by SHIMIZU Taku on 2019/02/25.
+//  Copyright © 2019 SHIMIZU Taku. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
