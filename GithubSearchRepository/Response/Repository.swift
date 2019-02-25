4ba1782bc11d453e337e4777ceb171e35848bec9
SHIMIZU Taku <shimizu.taku@gmail.com>
2019-02-25 15:35:50 +0900

    Initial commit

---
+++ GithubSearchRepository/Response/Repository.swift
@@ -0,0 +1,14 @@
+//
+//  Repository.swift
+//  GithubSearchRepository
+//
+//  Created by SHIMIZU Taku on 2019/02/25.
+//  Copyright © 2019 SHIMIZU Taku. All rights reserved.
+//
+
+struct Repository {
+    let id: Int
+    let name: String
+    let fullName: String
+    let owner: User
+}
---
+++ GithubSearchRepository/Response/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  GithubSearchRepository
+//
+//  Created by SHIMIZU Taku on 2019/02/25.
+//  Copyright © 2019 SHIMIZU Taku. All rights reserved.
+//
+
+struct User {
+    let id: Int
+    let login: String
+}
